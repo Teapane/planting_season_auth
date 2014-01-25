a3510d1b5a48f3100507e1044dacdba51ab6abec
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-24 19:14:37 -0700

    generate rails, remove extraneous pseudo code, add RSpec and FactoryGirl gems

---
+++ PlantingSeasonAuth/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlantingSeasonAuth::Application.config.secret_key_base = '6e882d76865774b5594a48b45250c89d3022c48c7c7ac215b6859f92a03bd48d276317d17f09ca5da8102313f4273c023ebd8f95d5f199cbf298557fa090fe82'
